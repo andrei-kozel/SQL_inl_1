34d2f7fcb72a5ab79e350e9373e90a2f8c521ad7
Andrei Kozel <kozel.andrei.94@gmail.com>
2021-02-09 21:48:03 +0100

    NEW: check if person exist

--- Genealogi/GenealogiCRUD.cs
+++ Genealogi/GenealogiCRUD.cs
@@ -56,7 +56,7 @@ namespace Genealogi
         }
 
         /// <summary>
-        /// Get a person from Database
+        /// Get a person from Database by name
         /// </summary>
         /// <param name="name">String Name</param>
         /// <returns>Person Object</returns>
@@ -66,6 +66,24 @@ namespace Genealogi
             db.OpenDatabase(DatabaseName);
             var dt = db.GetDataTable(@"SELECT TOP 1 * FROM People WHERE FIrstName LIKE @name", ("@name", name));
             if(dt.Rows.Count <= 0)
+            {
+                return null;
+            }
+
+            return GetPersonObject(dt.Rows[0]);
+        }
+
+        /// <summary>
+        /// Get a person from Database by Id
+        /// </summary>
+        /// <param name="id">Int Id</param>
+        /// <returns>Person Object</returns>
+        public Person Read(int id)
+        {
+            var db = new SQLDB();
+            db.OpenDatabase(DatabaseName);
+            var dt = db.GetDataTable(@"SELECT TOP 1 * FROM People WHERE Id LIKE @Id", ("@Id", id.ToString()));
+            if (dt.Rows.Count <= 0)
             {
                 return null;
             }
@@ -140,8 +158,33 @@ namespace Genealogi
                                 ("@Id", person.Id.ToString()));
         }
 
-        //public bool DoesPersonExist(string name) {/* Massor med kod */}
-        //public bool DoesPersonExist(int Id) {/* Massor med kod */}
+        /// <summary>
+        /// Check if the person exist. By Name
+        /// </summary>
+        /// <param name="name">String Name</param>
+        /// <returns>True or False</returns>
+        public bool DoesPersonExist(string name) {
+            if(Read(name) != null)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Check if the person exist. By ID
+        /// </summary>
+        /// <param name="id">Int Id</param>
+        /// <returns>True or False</returns>
+        public bool DoesPersonExist(int id) {
+            if (Read(id) != null)
+            {
+                return true;
+            }
+            return false;
+        }
+
+
         //public void GetFather(Person person) {/* Massor med kod */}
         //public void GetMother(Person person) {/* Massor med kod */}
         //public List<Person> List(string paramValue, string filter = "firstName LIKE @i nput") {/* Massor med kod */}
